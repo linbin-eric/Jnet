db54d46e4ea0f8b17501e664e96e6946a0fe49cd
eric <eric@jfire.cn>
2017-09-21 07:41:10 +0800

    心跳检测处理器重构

--- src/main/java/com/jfireframework/jnet/common/streamprocessor/HeartBeatProcessor.java
+++ src/main/java/com/jfireframework/jnet/common/streamprocessor/HeartBeatProcessor.java
@@ -12,55 +12,56 @@ import com.jfireframework.schedule.trigger.RepeatDelayTrigger;
 
 /**
  * 心跳检测处理器。
+ * 
  * @author linbin
  *
  */
 public class HeartBeatProcessor implements StreamProcessor
 {
-	private static final Timer TIMER;
-	static
-	{
-		TIMER = new FixedCapacityWheelTimer(1800, new SimpleExpireHandler(), Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()), 1, TimeUnit.SECONDS);
-	}
-	private final int		heartBeatDuration;
-	private volatile long	lastBeatTime;
-	
-	public HeartBeatProcessor(int heartBeatDuration)
-	{
-		this.heartBeatDuration = heartBeatDuration;
-	}
-	
-	@Override
-	public Object process(Object data, ChannelContext context) throws Throwable
-	{
-		lastBeatTime = System.currentTimeMillis();
-		return data;
-	}
-	
-	@Override
-	public void initialize(final ChannelContext channelContext)
-	{
-		lastBeatTime = System.currentTimeMillis();
-		TIMER.add(new RepeatDelayTrigger(new Timetask() {
-			boolean canceled = false;
-			
-			@Override
-			public boolean isCanceled()
-			{
-				return canceled;
-			}
-			
-			@Override
-			public void invoke()
-			{
-				long now = System.currentTimeMillis();
-				if (now - lastBeatTime > heartBeatDuration)
-				{
-					channelContext.close();
-					canceled = true;
-				}
-			}
-		}, heartBeatDuration, TimeUnit.SECONDS));
-	}
-	
+    private static final Timer TIMER;
+    static
+    {
+        TIMER = new FixedCapacityWheelTimer(1800, new SimpleExpireHandler(), Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()), 1, TimeUnit.SECONDS);
+    }
+    private final long    heartBeatDuration;
+    private volatile long lastBeatTime;
+    
+    public HeartBeatProcessor(long heartBeatDuration, TimeUnit unit)
+    {
+        this.heartBeatDuration = unit.toMillis(heartBeatDuration);
+    }
+    
+    @Override
+    public Object process(Object data, ChannelContext context) throws Throwable
+    {
+        lastBeatTime = System.currentTimeMillis();
+        return data;
+    }
+    
+    @Override
+    public void initialize(final ChannelContext channelContext)
+    {
+        lastBeatTime = System.currentTimeMillis();
+        TIMER.add(new RepeatDelayTrigger(new Timetask() {
+            boolean canceled = false;
+            
+            @Override
+            public boolean isCanceled()
+            {
+                return canceled;
+            }
+            
+            @Override
+            public void invoke()
+            {
+                long now = System.currentTimeMillis();
+                if (now - lastBeatTime > heartBeatDuration)
+                {
+                    channelContext.close();
+                    canceled = true;
+                }
+            }
+        }, heartBeatDuration, TimeUnit.MILLISECONDS));
+    }
+    
 }
