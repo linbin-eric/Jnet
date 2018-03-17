037d198bf4e1baf13946a0055ebbea50633fe088
eric <eric@jfire.cn>
2018-03-17 21:54:45 +0800

    使用内存池处理。

--- src/main/java/com/jfireframework/jnet/common/util/Allocator.java
+++ src/main/java/com/jfireframework/jnet/common/util/Allocator.java
@@ -2,11 +2,12 @@ package com.jfireframework.jnet.common.util;
 
 import com.jfireframework.pool.IoBufferAllocator;
 import com.jfireframework.pool.ioBuffer.IoBuffer;
+import com.jfireframework.pool.support.PooledIoBufferAllocator;
 import com.jfireframework.pool.support.UnPooledIoBufferAllocator;
 
 public class Allocator
 {
-	private static IoBufferAllocator allocator = new UnPooledIoBufferAllocator();
+	private static IoBufferAllocator allocator = new PooledIoBufferAllocator();
 	
 	public static IoBuffer allocate(int initSize)
 	{
