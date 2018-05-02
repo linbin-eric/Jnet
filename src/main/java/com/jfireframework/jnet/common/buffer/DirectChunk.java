5740da5cd483d5c2819602f30fa0117c446833d3
eric <eric@jfire.cn>
2018-05-02 19:27:06 +0800

    directChunk的扩容填充Buffer完成

--- src/main/java/com/jfireframework/jnet/common/buffer/DirectChunk.java
+++ src/main/java/com/jfireframework/jnet/common/buffer/DirectChunk.java
@@ -5,38 +5,45 @@ import com.jfireframework.jnet.common.util.Bits;
 
 class DirectChunk extends Chunk
 {
-	
-	private ByteBuffer buffer;
-	
-	public DirectChunk(int maxLevel, int unit)
-	{
-		super(maxLevel, unit);
-	}
-	
-	@Override
-	protected void initializeMem(int capacity)
-	{
-		buffer = ByteBuffer.allocateDirect(capacity);
-		address = Bits.getAddress(buffer);
-	}
-	
-	@Override
-	public boolean isDirect()
-	{
-		return true;
-	}
-	
-	@Override
-	protected void initBuffer(PooledIoBuffer buffer, int index, int off, int capacity)
-	{
-		buffer.setDirectIoBufferArgs(archon, this, index, address, off, this.buffer, capacity);
-	}
-	
-	@Override
-	protected void expansionBuffer(PooledIoBuffer buffer, int index, int off, int capacity)
-	{
-		// TODO Auto-generated method stub
-		
-	}
-	
+    
+    private ByteBuffer buffer;
+    
+    public DirectChunk(int maxLevel, int unit)
+    {
+        super(maxLevel, unit);
+    }
+    
+    @Override
+    protected void initializeMem(int capacity)
+    {
+        buffer = ByteBuffer.allocateDirect(capacity);
+        address = Bits.getAddress(buffer);
+    }
+    
+    @Override
+    public boolean isDirect()
+    {
+        return true;
+    }
+    
+    @Override
+    protected void initBuffer(PooledIoBuffer buffer, int index, int off, int capacity)
+    {
+        buffer.setDirectIoBufferArgs(archon, this, index, address, off, this.buffer, capacity);
+    }
+    
+    @Override
+    protected void expansionBuffer(PooledIoBuffer buffer, int index, int off, int capacity)
+    {
+        assert buffer.writePosi <= capacity;
+        Bits.copyDirectMemory(buffer.address + buffer.addressOffset, address + off, buffer.writePosi);
+        buffer.chunk = this;
+        buffer.index = index;
+        buffer.address = address;
+        buffer.addressOffset = off;
+        buffer.addressBuffer = this.buffer;
+        buffer.capacity = capacity;
+        buffer.internalByteBuffer = null;
+    }
+    
 }
