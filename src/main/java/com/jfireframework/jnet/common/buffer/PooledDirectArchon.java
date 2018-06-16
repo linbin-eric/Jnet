56b6312ae3871627387ba5ae8393c78f0f08acf2
eric <eric@jfire.cn>
2018-06-16 16:40:16 +0800

    增加了一个判断是否为Direct的方法

--- src/main/java/com/jfireframework/jnet/common/buffer/PooledDirectArchon.java
+++ src/main/java/com/jfireframework/jnet/common/buffer/PooledDirectArchon.java
@@ -4,45 +4,51 @@ import java.nio.ByteBuffer;
 
 public class PooledDirectArchon extends Archon
 {
-	
-	PooledDirectArchon(int maxLevel, int unit)
-	{
-		super(maxLevel, unit);
-	}
-	
-	@Override
-	protected void initHugeBuffer(PooledIoBuffer handler, int need)
-	{
-		ByteBuffer byteBuffer = ByteBuffer.allocateDirect(need);
-		long address = Bits.getAddress(byteBuffer);
-		handler.setDirectIoBufferArgs(this, null, -1, address, 0, byteBuffer, need);
-	}
-	
-	@Override
-	protected Chunk newChunk(int maxLevel, int unit)
-	{
-		return Chunk.newDirectChunk(maxLevel, unit);
-	}
-	
-	@Override
-	protected void expansionForHugeCapacity(PooledIoBuffer buffer, int newSize)
-	{
-		Chunk predChunk = buffer.chunk();
-		int predIndex = buffer.index;
-		ByteBuffer byteBuffer = ByteBuffer.allocateDirect(newSize);
-		long destAddress = Bits.getAddress(byteBuffer);
-		Bits.copyDirectMemory(buffer.address + buffer.addressOffset, destAddress, buffer.writePosi);
-		buffer.address = destAddress;
-		buffer.addressOffset = 0;
-		buffer.capacity = newSize;
-		buffer.addressBuffer = byteBuffer;
-		if (buffer.chunk() != null)
-		{
-			recycle(predChunk, predIndex);
-		}
-		buffer.chunk = null;
-		buffer.index = -1;
-		buffer.internalByteBuffer = null;
-	}
-	
+    
+    PooledDirectArchon(int maxLevel, int unit)
+    {
+        super(maxLevel, unit);
+    }
+    
+    @Override
+    protected void initHugeBuffer(PooledIoBuffer handler, int need)
+    {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(need);
+        long address = Bits.getAddress(byteBuffer);
+        handler.setDirectIoBufferArgs(this, null, -1, address, 0, byteBuffer, need);
+    }
+    
+    @Override
+    protected Chunk newChunk(int maxLevel, int unit)
+    {
+        return Chunk.newDirectChunk(maxLevel, unit);
+    }
+    
+    @Override
+    protected void expansionForHugeCapacity(PooledIoBuffer buffer, int newSize)
+    {
+        Chunk predChunk = buffer.chunk();
+        int predIndex = buffer.index;
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(newSize);
+        long destAddress = Bits.getAddress(byteBuffer);
+        Bits.copyDirectMemory(buffer.address + buffer.addressOffset, destAddress, buffer.writePosi);
+        buffer.address = destAddress;
+        buffer.addressOffset = 0;
+        buffer.capacity = newSize;
+        buffer.addressBuffer = byteBuffer;
+        if (buffer.chunk() != null)
+        {
+            recycle(predChunk, predIndex);
+        }
+        buffer.chunk = null;
+        buffer.index = -1;
+        buffer.internalByteBuffer = null;
+    }
+    
+    @Override
+    public boolean isDirect()
+    {
+        return true;
+    }
+    
 }
