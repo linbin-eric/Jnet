c2a07729048489bcb08fad970cd460d8f4272f13
eric <eric@jfire.cn>
2018-06-24 22:03:01 +0800

    先删除当前的Recycler实现。后续重新编写

--- src/test/java/com/jfireframework/jnet/test/recycler/FastThreadLocalTest.java
+++ src/test/java/com/jfireframework/jnet/test/recycler/FastThreadLocalTest.java
@@ -1,33 +1,48 @@
 package com.jfireframework.jnet.test.recycler;
 
 import static org.junit.Assert.assertEquals;
-import java.util.concurrent.locks.LockSupport;
+import static org.junit.Assert.assertFalse;
+import java.util.concurrent.CountDownLatch;
 import org.junit.Test;
 import com.jfireframework.jnet.common.thread.FastThreadLocal;
 import com.jfireframework.jnet.common.thread.FastThreadLocalThread;
 
 public class FastThreadLocalTest
 {
-	private FastThreadLocal<String> local = new FastThreadLocal<String>() {
-		@Override
-		protected String initializeValue()
-		{
-			return "123";
-		}
-	};
-	
-	@Test
-	public void test()
-	{
-		new FastThreadLocalThread(new Runnable() {
-			
-			@Override
-			public void run()
-			{
-				assertEquals("123", local.get());
-				assertEquals("123", local.get());
-			}
-		}).start();
-		LockSupport.parkNanos(10000000000l);
-	}
+    private FastThreadLocal<String> local = new FastThreadLocal<String>() {
+                                              @Override
+                                              protected String initializeValue()
+                                              {
+                                                  return "123";
+                                              }
+                                          };
+    boolean                         fail  = false;
+    
+    @Test
+    public void test() throws InterruptedException
+    {
+        final CountDownLatch latch = new CountDownLatch(1);
+        new FastThreadLocalThread(new Runnable() {
+            
+            @Override
+            public void run()
+            {
+                try
+                {
+                    assertEquals("123", local.get());
+                    assertEquals("123", local.get());
+                }
+                catch (Exception e)
+                {
+                    fail = true;
+                }
+                finally
+                {
+                    latch.countDown();
+                }
+            }
+        }).start();
+        latch.await();
+        assertFalse(fail);
+    }
 }
