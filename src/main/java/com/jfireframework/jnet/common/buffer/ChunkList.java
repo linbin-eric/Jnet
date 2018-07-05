f51ccd90a2e9f7cb12e3dbfa656b96cb6a529aec
eric <eric@jfire.cn>
2018-07-06 01:33:00 +0800

    修正完成了chunk在list之间的移动测试用例

--- src/main/java/com/jfireframework/jnet/common/buffer/ChunkList.java
+++ src/main/java/com/jfireframework/jnet/common/buffer/ChunkList.java
@@ -2,171 +2,174 @@ package com.jfireframework.jnet.common.buffer;
 
 public class ChunkList<T>
 {
-	final int		maxUsage;
-	final int		minUsage;
-	final int		maxReqCapacity;
-	ChunkList<T>	prevList;
-	ChunkList<T>	nextList;
-	Chunk<T>		head;
-	
-	/**
-	 * 两个边界都是闭区间。也就是大于等于最小使用率，小于等于最大使用率都在这个List中
-	 * 
-	 * @param minUsage
-	 * @param maxUsage
-	 * @param next
-	 * @param chunkSize
-	 */
-	public ChunkList(int minUsage, int maxUsage, ChunkList<T> next, int chunkSize)
-	{
-		this.maxUsage = maxUsage;
-		this.minUsage = minUsage;
-		maxReqCapacity = calcuteMaxCapacity(minUsage, chunkSize);
-		this.nextList = next;
-	}
-	
-	public void setPrevList(ChunkList<T> prevList)
-	{
-		this.prevList = prevList;
-	}
-	
-	int calcuteMaxCapacity(int minUsage, int chunkSize)
-	{
-		if (minUsage < 0)
-		{
-			return chunkSize;
-		}
-		else if (minUsage > 100)
-		{
-			return 0;
-		}
-		else
-		{
-			return (100 - minUsage) * chunkSize / 100;
-			
-		}
-	}
-	
-	public boolean allocate(int normalizeSize, PooledBuffer<T> buffer, ThreadCache cache)
-	{
-		if (head == null || normalizeSize >= maxReqCapacity)
-		{
-			return false;
-		}
-		Chunk<T> cursor = head;
-		long handle = cursor.allocate(normalizeSize);
-		if (handle != -1)
-		{
-			cursor.initBuf(handle, buffer, cache);
-			int usage = cursor.usage();
-			if (usage > maxUsage)
-			{
-				remove(cursor);
-				nextList.addFromPrev(cursor, usage);
-			}
-			return true;
-		}
-		while (cursor.next != null)
-		{
-			cursor = cursor.next;
-			handle = cursor.allocate(normalizeSize);
-			if (handle != -1)
-			{
-				cursor.initBuf(handle, buffer, cache);
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/**
-	 * 返回true意味着该Chunk不在管理之中，可以摧毁。
-	 * 
-	 * @param chunk
-	 * @param handle
-	 * @return
-	 */
-	public boolean free(Chunk<T> chunk, long handle)
-	{
-		chunk.free(handle);
-		int usage = chunk.usage();
-		if (usage < minUsage)
-		{
-			remove(chunk);
-			return addFromNext(chunk, usage) == false;
-		}
-		return false;
-	}
-	
-	void remove(Chunk<T> node)
-	{
-		Chunk<T> head = this.head;
-		if (node == head)
-		{
-			head = node.next;
-			if (head != null)
-			{
-				head.pred = null;
-			}
-			this.head = head;
-		}
-		else
-		{
-			Chunk<T> next = node.next;
-			node.pred.next = next;
-			if (next != null)
-			{
-				next.pred = node.pred;
-			}
-		}
-	}
-	
-	/**
-	 * 返回true意味着有前向列表可以添加，否则返回false
-	 * 
-	 * @param chunk
-	 * @param usage
-	 * @return
-	 */
-	public boolean addFromNext(Chunk<T> chunk, int usage)
-	{
-		if (prevList == null)
-		{
-			return false;
-		}
-		if (usage <= minUsage)
-		{
-			return prevList.addFromNext(chunk, usage);
-		}
-		add(chunk);
-		return true;
-	}
-	
-	void add(Chunk<T> chunk)
-	{
-		chunk.parent = this;
-		if (head == null)
-		{
-			head = chunk;
-			chunk.pred = null;
-			chunk.next = null;
-		}
-		else
-		{
-			chunk.pred = null;
-			chunk.next = head;
-			head.pred = chunk;
-			head = chunk;
-		}
-	}
-	
-	public void addFromPrev(Chunk<T> chunk, int usage)
-	{
-		if (usage >= maxUsage)
-		{
-			nextList.addFromPrev(chunk, usage);
-			return;
-		}
-		add(chunk);
-	}
+    final int    maxUsage;
+    final int    minUsage;
+    final int    maxReqCapacity;
+    ChunkList<T> prevList;
+    ChunkList<T> nextList;
+    Chunk<T>     head;
+    
+    /**
+     * 两个边界都是闭区间。也就是大于等于最小使用率，小于等于最大使用率都在这个List中
+     * 
+     * @param minUsage
+     * @param maxUsage
+     * @param next
+     * @param chunkSize
+     */
+    public ChunkList(int minUsage, int maxUsage, ChunkList<T> next, int chunkSize)
+    {
+        this.maxUsage = maxUsage;
+        this.minUsage = minUsage;
+        maxReqCapacity = calcuteMaxCapacity(minUsage, chunkSize);
+        this.nextList = next;
+    }
+    
+    public void setPrevList(ChunkList<T> prevList)
+    {
+        this.prevList = prevList;
+    }
+    
+    int calcuteMaxCapacity(int minUsage, int chunkSize)
+    {
+        if (minUsage < 0)
+        {
+            return chunkSize;
+        }
+        else if (minUsage > 100)
+        {
+            return 0;
+        }
+        else
+        {
+            return (100 - minUsage) * chunkSize / 100;
+            
+        }
+    }
+    
+    public boolean allocate(int normalizeSize, PooledBuffer<T> buffer, ThreadCache cache)
+    {
+        if (head == null || normalizeSize >= maxReqCapacity)
+        {
+            return false;
+        }
+        Chunk<T> cursor = head;
+        long handle = cursor.allocate(normalizeSize);
+        if (handle != -1)
+        {
+            cursor.initBuf(handle, buffer, cache);
+            int usage = cursor.usage();
+            if (usage > maxUsage)
+            {
+                remove(cursor);
+                nextList.addFromPrev(cursor, usage);
+            }
+            return true;
+        }
+        while (cursor.next != null)
+        {
+            cursor = cursor.next;
+            handle = cursor.allocate(normalizeSize);
+            if (handle != -1)
+            {
+                cursor.initBuf(handle, buffer, cache);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * 返回true意味着该Chunk不在管理之中，可以摧毁。
+     * 
+     * @param chunk
+     * @param handle
+     * @return
+     */
+    public boolean free(Chunk<T> chunk, long handle)
+    {
+        chunk.free(handle);
+        int usage = chunk.usage();
+        if (usage < minUsage)
+        {
+            remove(chunk);
+            return addFromNext(chunk, usage) == false;
+        }
+        return false;
+    }
+    
+    void remove(Chunk<T> node)
+    {
+        Chunk<T> head = this.head;
+        if (node == head)
+        {
+            head = node.next;
+            if (head != null)
+            {
+                head.pred = null;
+            }
+            this.head = head;
+        }
+        else
+        {
+            Chunk<T> next = node.next;
+            node.pred.next = next;
+            if (next != null)
+            {
+                next.pred = node.pred;
+            }
+        }
+    }
+    
+    /**
+     * 返回true意味着有前向列表可以添加，否则返回false
+     * 
+     * @param chunk
+     * @param usage
+     * @return
+     */
+    public boolean addFromNext(Chunk<T> chunk, int usage)
+    {
+        if (usage < minUsage)
+        {
+            if (prevList == null)
+            {
+                return false;
+            }
+            else
+            {
+                return prevList.addFromNext(chunk, usage);
+            }
+        }
+        add(chunk);
+        return true;
+    }
+    
+    void add(Chunk<T> chunk)
+    {
+        chunk.parent = this;
+        if (head == null)
+        {
+            head = chunk;
+            chunk.pred = null;
+            chunk.next = null;
+        }
+        else
+        {
+            chunk.pred = null;
+            chunk.next = head;
+            head.pred = chunk;
+            head = chunk;
+        }
+    }
+    
+    public void addFromPrev(Chunk<T> chunk, int usage)
+    {
+        if (usage > maxUsage)
+        {
+            nextList.addFromPrev(chunk, usage);
+            return;
+        }
+        add(chunk);
+    }
 }
--- src/main/java/com/jfireframework/jnet/common/buffer/ThreadCache.java
+++ src/main/java/com/jfireframework/jnet/common/buffer/ThreadCache.java
@@ -6,160 +6,173 @@ import com.jfireframework.jnet.common.util.MathUtil;
 
 public class ThreadCache
 {
-	final HeapArena							heapArena;
-	final MemoryRegionCache<byte[]>[]		tinySubPagesHeapCaches;
-	final MemoryRegionCache<byte[]>[]		smallSubPageHeapCaches;
-	final MemoryRegionCache<byte[]>[]		normalHeapCaches;
-	final DirectArena						directArena;
-	final MemoryRegionCache<ByteBuffer>[]	tinySubPagesDirectCaches;
-	final MemoryRegionCache<ByteBuffer>[]	smallSubPagesDirectCaches;
-	final MemoryRegionCache<ByteBuffer>[]	normalDirectCaches;
-	final int								pageSizeShift;
-	
-	@SuppressWarnings("unchecked")
-	public ThreadCache(HeapArena heapArena, DirectArena directArena, int tinyCacheSize, int smallCacheSize, int normalCacheSize, int maxCachedBufferCapacity, int pageSizeShift)
-	{
-		this.pageSizeShift = pageSizeShift;
-		this.heapArena = heapArena;
-		tinySubPagesHeapCaches = new MemoryRegionCache[heapArena.tinySubPages.length];
-		for (int i = 0; i < tinySubPagesHeapCaches.length; i++)
-		{
-			tinySubPagesHeapCaches[i] = new MemoryRegionCache<>(tinyCacheSize);
-		}
-		smallSubPageHeapCaches = new MemoryRegionCache[heapArena.smallSubPages.length];
-		for (int i = 0; i < smallSubPageHeapCaches.length; i++)
-		{
-			smallSubPageHeapCaches[i] = new MemoryRegionCache<>(smallCacheSize);
-		}
-		int normalizeSize = MathUtil.normalizeSize(maxCachedBufferCapacity);
-		int shift = MathUtil.log2(normalizeSize);
-		normalHeapCaches = new MemoryRegionCache[shift - pageSizeShift];
-		for (int i = 0; i < normalHeapCaches.length; i++)
-		{
-			normalHeapCaches[i] = new MemoryRegionCache<>(normalCacheSize);
-		}
-		this.directArena = directArena;
-		tinySubPagesDirectCaches = new MemoryRegionCache[directArena.tinySubPages.length];
-		for (int i = 0; i < tinySubPagesDirectCaches.length; i++)
-		{
-			tinySubPagesDirectCaches[i] = new MemoryRegionCache<>(tinyCacheSize);
-		}
-		smallSubPagesDirectCaches = new MemoryRegionCache[directArena.smallSubPages.length];
-		for (int i = 0; i < smallSubPagesDirectCaches.length; i++)
-		{
-			smallSubPagesDirectCaches[i] = new MemoryRegionCache<>(smallCacheSize);
-		}
-		normalDirectCaches = new MemoryRegionCache[shift - pageSizeShift];
-		for (int i = 0; i < normalDirectCaches.length; i++)
-		{
-			normalDirectCaches[i] = new MemoryRegionCache<>(normalCacheSize);
-		}
-	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public boolean allocate(PooledBuffer<?> buffer, int normalizeCapacity, SizeType sizeType, Arena<?> arena)
-	{
-		MemoryRegionCache<?> cache = findCache(normalizeCapacity, sizeType, arena);
-		if (cache == null)
-		{
-			return false;
-		}
-		return ((MemoryRegionCache) cache).tryFindAndInitBuffer(buffer, this);
-		
-	}
-	
-	private MemoryRegionCache<?> findCache(int normalizeCapacity, SizeType sizeType, Arena<?> arena)
-	{
-		MemoryRegionCache<?> cache = null;
-		switch (sizeType)
-		{
-			case TINY:
-				cache = cacheForTiny(normalizeCapacity, arena);
-				break;
-			case SMALL:
-				cache = cacheForSmall(normalizeCapacity, arena);
-				break;
-			case NORMAL:
-				cache = cacheForNormal(normalizeCapacity, arena);
-				break;
-			default:
-				ReflectUtil.throwException(new NullPointerException());
-		}
-		return cache;
-	}
-	
-	MemoryRegionCache<?> cacheForTiny(int normalizeCapacity, Arena<?> arena)
-	{
-		int tinyIdx = Arena.tinyIdx(normalizeCapacity);
-		if (arena.isDirect())
-		{
-			if (tinySubPagesDirectCaches == null || tinySubPagesDirectCaches.length <= tinyIdx)
-			{
-				return null;
-			}
-			return tinySubPagesDirectCaches[tinyIdx];
-		}
-		else
-		{
-			if (tinySubPagesHeapCaches == null || tinySubPagesHeapCaches.length <= tinyIdx)
-			{
-				return null;
-			}
-			return tinySubPagesHeapCaches[tinyIdx];
-		}
-	}
-	
-	MemoryRegionCache<?> cacheForSmall(int normalizeCapacity, Arena<?> arena)
-	{
-		int smallIdx = Arena.smallIdx(normalizeCapacity);
-		if (arena.isDirect())
-		{
-			if (smallSubPagesDirectCaches == null || smallSubPageHeapCaches.length <= smallIdx)
-			{
-				return null;
-			}
-			return smallSubPagesDirectCaches[smallIdx];
-		}
-		else
-		{
-			if (smallSubPageHeapCaches == null || smallSubPageHeapCaches.length <= smallIdx)
-			{
-				return null;
-			}
-			return smallSubPageHeapCaches[smallIdx];
-		}
-	}
-	
-	MemoryRegionCache<?> cacheForNormal(int normalizeCapacity, Arena<?> arena)
-	{
-		int shift = MathUtil.log2(normalizeCapacity);
-		int idx = shift - pageSizeShift;
-		if (arena.isDirect())
-		{
-			if (normalDirectCaches == null || normalDirectCaches.length <= idx)
-			{
-				return null;
-			}
-			return normalDirectCaches[idx];
-		}
-		else
-		{
-			if (normalHeapCaches == null || normalHeapCaches.length <= idx)
-			{
-				return null;
-			}
-			return normalHeapCaches[idx];
-		}
-	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public boolean add(int normalizeCapacity, SizeType sizeType, Arena<?> arena, Chunk<?> chunk, long handle)
-	{
-		MemoryRegionCache<?> cache = findCache(normalizeCapacity, sizeType, arena);
-		if (cache == null)
-		{
-			return false;
-		}
-		return ((MemoryRegionCache) cache).offer(chunk, handle);
-	}
+    final HeapArena                       heapArena;
+    final MemoryRegionCache<byte[]>[]     tinySubPagesHeapCaches;
+    final MemoryRegionCache<byte[]>[]     smallSubPageHeapCaches;
+    final MemoryRegionCache<byte[]>[]     normalHeapCaches;
+    final DirectArena                     directArena;
+    final MemoryRegionCache<ByteBuffer>[] tinySubPagesDirectCaches;
+    final MemoryRegionCache<ByteBuffer>[] smallSubPagesDirectCaches;
+    final MemoryRegionCache<ByteBuffer>[] normalDirectCaches;
+    final int                             pageSizeShift;
+    
+    public ThreadCache(HeapArena heapArena, DirectArena directArena, int tinyCacheSize, int smallCacheSize, int normalCacheSize, int maxCachedBufferCapacity, int pageSizeShift)
+    {
+        this.pageSizeShift = pageSizeShift;
+        this.heapArena = heapArena;
+        tinySubPagesHeapCaches = createSubPageMemoryRegionCache(tinyCacheSize);
+        smallSubPageHeapCaches = createSubPageMemoryRegionCache(smallCacheSize);
+        normalHeapCaches = createNormalSizeMemoryRegionCache(normalCacheSize, pageSizeShift, maxCachedBufferCapacity);
+        this.directArena = directArena;
+        tinySubPagesDirectCaches = createSubPageMemoryRegionCache(normalCacheSize);
+        smallSubPagesDirectCaches = createSubPageMemoryRegionCache(normalCacheSize);
+        normalDirectCaches = createNormalSizeMemoryRegionCache(normalCacheSize, pageSizeShift, maxCachedBufferCapacity);
+    }
+    
+    @SuppressWarnings("unchecked")
+    private <T> MemoryRegionCache<T>[] createSubPageMemoryRegionCache(int cacheSize)
+    {
+        if (cacheSize > 0)
+        {
+            MemoryRegionCache<T>[] array = new MemoryRegionCache[cacheSize];
+            for (int i = 0; i < array.length; i++)
+            {
+                array[i] = new MemoryRegionCache<>(cacheSize);
+            }
+            return array;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    private <T> MemoryRegionCache<T>[] createNormalSizeMemoryRegionCache(int cacheSize, int pagesizeShift, int maxCachedBufferCapacity)
+    {
+        int normalizeSize = MathUtil.normalizeSize(maxCachedBufferCapacity);
+        int shift = MathUtil.log2(normalizeSize);
+        int length = shift - normalizeSize;
+        if (length > 0 && cacheSize > 0)
+        {
+            MemoryRegionCache<T>[] array = new MemoryRegionCache[length];
+            for (int i = 0; i < array.length; i++)
+            {
+                array[i] = new MemoryRegionCache<>(cacheSize);
+            }
+            return array;
+            
+        }
+        else
+        {
+            return null;
+        }
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public boolean allocate(PooledBuffer<?> buffer, int normalizeCapacity, SizeType sizeType, Arena<?> arena)
+    {
+        MemoryRegionCache<?> cache = findCache(normalizeCapacity, sizeType, arena);
+        if (cache == null)
+        {
+            return false;
+        }
+        return ((MemoryRegionCache) cache).tryFindAndInitBuffer(buffer, this);
+        
+    }
+    
+    private MemoryRegionCache<?> findCache(int normalizeCapacity, SizeType sizeType, Arena<?> arena)
+    {
+        MemoryRegionCache<?> cache = null;
+        switch (sizeType)
+        {
+            case TINY:
+                cache = cacheForTiny(normalizeCapacity, arena);
+                break;
+            case SMALL:
+                cache = cacheForSmall(normalizeCapacity, arena);
+                break;
+            case NORMAL:
+                cache = cacheForNormal(normalizeCapacity, arena);
+                break;
+            default:
+                ReflectUtil.throwException(new NullPointerException());
+        }
+        return cache;
+    }
+    
+    MemoryRegionCache<?> cacheForTiny(int normalizeCapacity, Arena<?> arena)
+    {
+        int tinyIdx = Arena.tinyIdx(normalizeCapacity);
+        if (arena.isDirect())
+        {
+            if (tinySubPagesDirectCaches == null || tinySubPagesDirectCaches.length <= tinyIdx)
+            {
+                return null;
+            }
+            return tinySubPagesDirectCaches[tinyIdx];
+        }
+        else
+        {
+            if (tinySubPagesHeapCaches == null || tinySubPagesHeapCaches.length <= tinyIdx)
+            {
+                return null;
+            }
+            return tinySubPagesHeapCaches[tinyIdx];
+        }
+    }
+    
+    MemoryRegionCache<?> cacheForSmall(int normalizeCapacity, Arena<?> arena)
+    {
+        int smallIdx = Arena.smallIdx(normalizeCapacity);
+        if (arena.isDirect())
+        {
+            if (smallSubPagesDirectCaches == null || smallSubPageHeapCaches.length <= smallIdx)
+            {
+                return null;
+            }
+            return smallSubPagesDirectCaches[smallIdx];
+        }
+        else
+        {
+            if (smallSubPageHeapCaches == null || smallSubPageHeapCaches.length <= smallIdx)
+            {
+                return null;
+            }
+            return smallSubPageHeapCaches[smallIdx];
+        }
+    }
+    
+    MemoryRegionCache<?> cacheForNormal(int normalizeCapacity, Arena<?> arena)
+    {
+        int shift = MathUtil.log2(normalizeCapacity);
+        int idx = shift - pageSizeShift;
+        if (arena.isDirect())
+        {
+            if (normalDirectCaches == null || normalDirectCaches.length <= idx)
+            {
+                return null;
+            }
+            return normalDirectCaches[idx];
+        }
+        else
+        {
+            if (normalHeapCaches == null || normalHeapCaches.length <= idx)
+            {
+                return null;
+            }
+            return normalHeapCaches[idx];
+        }
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public boolean add(int normalizeCapacity, SizeType sizeType, Arena<?> arena, Chunk<?> chunk, long handle)
+    {
+        MemoryRegionCache<?> cache = findCache(normalizeCapacity, sizeType, arena);
+        if (cache == null)
+        {
+            return false;
+        }
+        return ((MemoryRegionCache) cache).offer(chunk, handle);
+    }
 }
