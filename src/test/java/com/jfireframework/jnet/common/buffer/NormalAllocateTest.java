ec59c4220a9e69b63eaa1521ce4e24cebf3fa6ab
eric <eric@jfire.cn>
2018-07-08 20:22:42 +0800

    修正了正常分配的测试用例

--- src/test/java/com/jfireframework/jnet/common/buffer/NormalAllocateTest.java
+++ src/test/java/com/jfireframework/jnet/common/buffer/NormalAllocateTest.java
@@ -7,53 +7,34 @@ import org.junit.Test;
 
 public class NormalAllocateTest
 {
-	int						pagesize	= 1024;
-	PooledBufferAllocator	allocator	= new PooledBufferAllocator(pagesize, 4, 1, 1, 0, 0, 0, 0, false, true);
-	
-	@Test
-	public void test()
-	{
-		test0(true);
-		test0(false);
-	}
-	
-	private void test0(boolean direct)
-	{
-		List<IoBuffer> buffers = new LinkedList<>();
-		for (int i = 0; i < 16; i++)
-		{
-			PooledBuffer<?> ioBuffer = (PooledBuffer<?>) allocator.ioBuffer(pagesize, direct);
-			long handle = ioBuffer.handle;
-			assertEquals(16 + i, handle);
-			buffers.add(ioBuffer);
-		}
-		for (int i = 0; i < 8; i++)
-		{
-			PooledBuffer<?> ioBuffer = (PooledBuffer<?>) allocator.ioBuffer(pagesize << 1, direct);
-			long handle = ioBuffer.handle;
-			assertEquals(8 + i, handle);
-			buffers.add(ioBuffer);
-		}
-		for (int i = 0; i < 4; i++)
-		{
-			PooledBuffer<?> ioBuffer = (PooledBuffer<?>) allocator.ioBuffer(pagesize << 2, direct);
-			long handle = ioBuffer.handle;
-			assertEquals(4 + i, handle);
-			buffers.add(ioBuffer);
-		}
-		for (int i = 0; i < 2; i++)
-		{
-			PooledBuffer<?> ioBuffer = (PooledBuffer<?>) allocator.ioBuffer(pagesize << 3, direct);
-			long handle = ioBuffer.handle;
-			assertEquals(2 + i, handle);
-			buffers.add(ioBuffer);
-		}
-		for (int i = 0; i < 1; i++)
-		{
-			PooledBuffer<?> ioBuffer = (PooledBuffer<?>) allocator.ioBuffer(pagesize << 4, direct);
-			long handle = ioBuffer.handle;
-			assertEquals(1 + i, handle);
-			buffers.add(ioBuffer);
-		}
-	}
+    PooledBufferAllocator allocator = new PooledBufferAllocator(PooledBufferAllocator.PAGESIZE, PooledBufferAllocator.MAXLEVEL, 1, 1, 0, 0, 0, 0, false, true);
+    
+    @Test
+    public void test()
+    {
+        test0(true);
+        test0(false);
+    }
+    
+    private void test0(boolean direct)
+    {
+        List<IoBuffer> buffers = new LinkedList<>();
+        for (int i = allocator.maxLevel; i >= 0; i--)
+        {
+            int levelSize = allocator.pagesize << (allocator.maxLevel - i);
+            int base = 1 << i;
+            for (int j = 0; j < 1 << i; j++)
+            {
+                PooledBuffer<?> buffer = (PooledBuffer<?>) allocator.ioBuffer(levelSize, direct);
+                long handle = buffer.handle;
+                assertEquals(base + j, handle);
+                buffers.add(buffer);
+            }
+            for (IoBuffer each : buffers)
+            {
+                each.free();
+            }
+            buffers.clear();
+        }
+    }
 }
