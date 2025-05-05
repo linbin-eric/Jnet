87993abd6ea5f818a8021bbac7c01df009b1f946
linbin <495561397@qq.com>
2025-05-06 01:04:41 +0800

    优化了自动设置类型和长度

--- src/main/java/com/jfirer/jnet/extend/http/decode/HttpRespEncoder.java
+++ src/main/java/com/jfirer/jnet/extend/http/decode/HttpRespEncoder.java
@@ -12,12 +12,13 @@ import com.jfirer.jnet.extend.http.dto.HttpRespHead;
 
 import java.nio.charset.StandardCharsets;
 
-public class HttpRespPartEncoder implements WriteProcessor<Object>
+public class HttpRespEncoder implements WriteProcessor<Object>
 {
     private final       BufferAllocator allocator;
-    public static final byte[]          NEWLINE = "\r\n".getBytes(StandardCharsets.US_ASCII);
+    public static final byte[]          NEWLINE          = "\r\n".getBytes(StandardCharsets.US_ASCII);
+    public static final byte[]          APPLICATION_JSON = "content-type: application/json;charset=utf8\r\n".getBytes(StandardCharsets.US_ASCII);
 
-    public HttpRespPartEncoder(BufferAllocator allocator)
+    public HttpRespEncoder(BufferAllocator allocator)
     {
         this.allocator = allocator;
     }
@@ -28,13 +29,14 @@ public class HttpRespPartEncoder implements WriteProcessor<Object>
         if (obj instanceof HttpRespHead head)
         {
             IoBuffer buffer = allocator.ioBuffer(1024);
-            putHead(head, buffer);
+            head.write(buffer);
+            buffer.put(NEWLINE);
             next.fireWrite(buffer);
         }
         else if (obj instanceof HttpRespBody body)
         {
             IoBuffer buffer = allocator.ioBuffer(1024);
-            putBody(body, buffer);
+            putBody(body, buffer, false);
             next.fireWrite(buffer);
         }
         else if (obj instanceof FullHttpResp fullHttpResp)
@@ -42,11 +44,12 @@ public class HttpRespPartEncoder implements WriteProcessor<Object>
             IoBuffer buffer = allocator.ioBuffer(1024);
             try
             {
-                putHead(fullHttpResp.getHead(), buffer);
-                if (fullHttpResp.getBody().isEmpty() == false)
+                fullHttpResp.getHead().write(buffer);
+                if (fullHttpResp.getHead().hasContentType() == false)
                 {
-                    putBody(fullHttpResp.getBody(), buffer);
+                    buffer.put(APPLICATION_JSON);
                 }
+                putBody(fullHttpResp.getBody(), buffer, fullHttpResp.getHead().hasContentLength() == false);
                 next.fireWrite(buffer);
             }
             catch (Throwable e)
@@ -64,31 +67,40 @@ public class HttpRespPartEncoder implements WriteProcessor<Object>
         }
     }
 
-    private void putBody(HttpRespBody body, IoBuffer buffer)
+    private void putBody(HttpRespBody body, IoBuffer buffer, boolean setLength)
     {
         if (body.getBodyBuffer() != null && body.getBodyBuffer().remainRead() > 0)
         {
+            if (setLength)
+            {
+                buffer.put(STR.format("Content-Length: {}\r\n", body.getBodyBuffer().remainRead()).getBytes(StandardCharsets.US_ASCII));
+                buffer.put(NEWLINE);
+            }
             buffer.put(body.getBodyBuffer());
             body.getBodyBuffer().free();
         }
         else if (body.getBodyBytes() != null)
         {
+            if (setLength)
+            {
+                buffer.put(STR.format("Content-Length: {}\r\n", body.getBodyBytes().length).getBytes(StandardCharsets.US_ASCII));
+                buffer.put(NEWLINE);
+            }
             buffer.put(body.getBodyBytes());
         }
         else if (body.getBodyText() != null)
         {
-            buffer.put(body.getBodyText().getBytes(StandardCharsets.UTF_8));
+            byte[] array = body.getBodyText().getBytes(StandardCharsets.UTF_8);
+            if (setLength)
+            {
+                buffer.put(STR.format("Content-Length: {}\r\n", array.length).getBytes(StandardCharsets.US_ASCII));
+                buffer.put(NEWLINE);
+            }
+            buffer.put(array);
         }
         else
         {
             throw new IllegalArgumentException("body参数内容是空的");
         }
-    }
-
-    private static void putHead(HttpRespHead head, IoBuffer buffer)
-    {
-        buffer.put(("HTTP/1.1 " + head.getResponseCode() + " OK\n").getBytes(StandardCharsets.US_ASCII));
-        head.getHeaders().forEach((name, value) -> buffer.put((name + ": " + value + "\r\n").getBytes(StandardCharsets.US_ASCII)));
-        buffer.put(NEWLINE);
     }
 }
--- src/main/java/com/jfirer/jnet/extend/http/dto/HttpRespHead.java
+++ src/main/java/com/jfirer/jnet/extend/http/dto/HttpRespHead.java
@@ -1,22 +1,45 @@
 package com.jfirer.jnet.extend.http.dto;
 
-import lombok.Data;
-import lombok.experimental.Accessors;
+import com.jfirer.jnet.common.buffer.buffer.IoBuffer;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
-@Data
-@Accessors(chain = true)
 public class HttpRespHead implements HttpRespPart
 {
-    private int                 responseCode = 200;
-    private Map<String, String> headers      = new HashMap<>();
+    private int                 responseCode     = 200;
+    private Map<String, String> headers          = new HashMap<>();
+    private boolean             hasContentLength = false;
+    private boolean             hasContentType   = false;
 
     public HttpRespHead addHeader(String name, String value)
     {
+        if (name.toLowerCase().startsWith("content-length"))
+        {
+            hasContentLength = true;
+        }
+        if (name.toLowerCase().startsWith("content-type"))
+        {
+            hasContentType = true;
+        }
         headers.put(name, value);
         return this;
     }
 
+    public boolean hasContentLength()
+    {
+        return hasContentLength;
+    }
+
+    public boolean hasContentType()
+    {
+        return hasContentType;
+    }
+
+    public void write(IoBuffer buffer)
+    {
+        buffer.put(("HTTP/1.1 " + responseCode + " OK\n").getBytes(StandardCharsets.US_ASCII));
+        headers.forEach((name, value) -> buffer.put((name + ": " + value + "\r\n").getBytes(StandardCharsets.US_ASCII)));
+    }
 }
