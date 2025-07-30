53a61f5f746f063a4132f884b2eaa527dc00e96d
linbin <495561397@qq.com>
2025-07-31 04:20:14 +0800

    新增背压的两个实际实现。，

---
+++ src/main/java/com/jfirer/jnet/extend/watercheck/WriteLimiter.java
@@ -0,0 +1,53 @@
+package com.jfirer.jnet.extend.watercheck;
+
+import com.jfirer.jnet.common.api.WriteListener;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class WriteLimiter implements WriteListener
+{
+    private volatile AtomicLong  counter;
+    private volatile ReadLimiter readLimiter;
+
+    @Override
+    public void queuedWrite(long size)
+    {
+        AtomicLong c = counter;
+        if (c != null)
+        {
+            c.addAndGet(size);
+        }
+    }
+
+    @Override
+    public void partWriteFinish(long currentSend)
+    {
+        AtomicLong c = counter;
+        if (c != null)
+        {
+            long        left = c.addAndGet(0 - currentSend);
+            ReadLimiter r    = readLimiter;
+            if (left < ReadLimiter.MAX_QUEUED_SIZE && r != null)
+            {
+                r.tryRegisterRead();
+            }
+        }
+    }
+
+    @Override
+    public void writeFailed(Throwable e)
+    {
+        ReadLimiter r = readLimiter;
+        if (r != null)
+        {
+            r.tryRegisterRead();
+        }
+    }
+
+    public WriteLimiter associated(ReadLimiter readLimiter)
+    {
+        this.readLimiter = readLimiter;
+        counter          = readLimiter.getCounter();
+        return this;
+    }
+}
